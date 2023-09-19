9501722ca30632fa90aa90c1eba03e5bdd47cc27
koju2005 <141552941+koju2005@users.noreply.github.com>
2023-09-19 16:48:56 +0900

    스텟버튼UI 출력,미출력 구현

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public GameManager i;
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void Awake()
     {
-        
+        i = this;
     }
 }
